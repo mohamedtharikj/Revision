416b01471c50f27dddb1d578d2a59484dda31471
mohamed <tharikt720@gmail.com>
2022-09-08 18:17:54 +0530

    prince done his coding

--- src/Org/Sample/Sample.java
+++ src/Org/Sample/Sample.java
@@ -10,10 +10,16 @@ private void age() {
 	System.out.println("my age is 22");
 }
 
+private void phoneno() {
+	System.out.println("8608636741");
+
+}
+
 public static void main(String[] args) {
 	Sample s=new Sample();
 	s.name();
 	s.age();
+	s.phoneno();
 }
 
 }
