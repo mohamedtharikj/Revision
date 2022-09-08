2f12930796e182d788b55483c85447607d88369a
mohamed <tharikt720@gmail.com>
2022-09-08 17:52:40 +0530

    Done

---
+++ src/Org/Sample/Sample.java
@@ -0,0 +1,19 @@
+package Org.Sample;
+
+public class Sample {
+	
+private void name() {
+	System.out.println("my name is tharik");
+
+}
+private void age() {
+	System.out.println("my age is 22");
+}
+
+public static void main(String[] args) {
+	Sample s=new Sample();
+	s.name();
+	s.age();
+}
+
+}
